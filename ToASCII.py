5e17959d3d306b065f3c2567956e95b4fbdc19bf
Ian <ian@wushuworks.com>
2024-10-20 06:16:23 -0700

    added error explanation line

--- ToASCII.py
+++ ToASCII.py
@@ -247,7 +247,8 @@ def _safe_divied(x,y, default = 0):
     """
     try:
         return x / y
-    except ArithmeticError:
+    except ArithmeticError as err:
+        print(f"Bad division: {err}")
         return default
         
 def _clamp(value, min_value, max_value) :
